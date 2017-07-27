34a574b4310b99b13d0d23b3dd13643bb99a0ddd
losephjambert <josephc.lambert@gmail.com>
2017-07-27 10:08:52 -0700

    gets entry by entry_id

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,8 @@ const contentful = require('contentful')
 
 module.exports = function(req, res) {
   const client = contentful.createClient({
-    space: process.env.SPACE_ID,
-    accessToken: process.env.ACCESS_TOKEN
+    space: process.env.SPACE_ID || '3rtn1vfpfic2',
+    accessToken: process.env.ACCESS_TOKEN || '22eb6ce953fda24b10d7b4e693685c4891063361aff3dae19136c1656792d36c'
   })
 
   client.getEntries({content_type: 'blogPost'})
--- routes/project.js
+++ routes/project.js
@@ -1,15 +1,15 @@
 const contentful = require('contentful')
 
 module.exports = function(req, res) {
-  const entry_id = req.query
+  const entry_id = req.query.projectId
   const client = contentful.createClient({
-    space: process.env.SPACE_ID,
-    accessToken: process.env.ACCESS_TOKEN
+    space: process.env.SPACE_ID || '3rtn1vfpfic2',
+    accessToken: process.env.ACCESS_TOKEN || '22eb6ce953fda24b10d7b4e693685c4891063361aff3dae19136c1656792d36c'
   })
 
   client.getEntry(entry_id)
     .then((response) =>
-      res.send(response.fields)
+      res.send(response)
     )
     .catch(console.error)
 }
