7b8875da3b5383da00d313c37f37d591c4e644cc
losephjambert <josephc.lambert@gmail.com>
2017-07-26 22:19:28 -0700

    remove config

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const contentful = require('contentful');
 
 module.exports = function(req, res) {
   const client = contentful.createClient({
-    space: process.env.SPACE_ID
+    space: process.env.SPACE_ID,
     accessToken: process.env.ACCESS_TOKEN
   })
 
