9aea9d0e0a724c47f338069b20ee151504325007
losephjambert <josephc.lambert@gmail.com>
2017-07-26 18:48:42 -0700

    gets the data

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,15 @@
 const contentful = require('contentful');
-const config = require("./.config/contentful.js");
+const config = require("../.config/contentful.js");
 
 module.exports = function(req, res) {
   const client = contentful.createClient({
     space: process.env.SPACE_ID || config.space,
     accessToken: process.env.ACCESS_TOKEN || config.accessToken
   })
-  client.getEntries(client.accessToken)
-  .then(function (entries) {
-    console.log(entries)
-  })
 
+  client.getEntries({content_type: 'blogPost'})
+    .then((response) =>
+      res.send(response.items)
+    )
+    .catch(console.error)
 }
