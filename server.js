d529ca8e670608327a093d87f1afed4d5c33054f
losephjambert <josephc.lambert@gmail.com>
2017-07-26 18:54:31 -0700

    comment out body parser

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 // SET PORT
 const port = process.env.PORT || 8080;
 
 // USE BODYPARSER I GUESS
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
 
 // SET ACCESS CONTROL HEADERS FOR DEV
 app.use(function(req, res, next) {
